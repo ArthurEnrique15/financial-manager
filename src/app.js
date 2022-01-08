789687c38e29c3784c76966d73599d8709299897
ArthurEnrique15 <arthurenrique1512@gmail.com>
2022-01-08 19:47:43 -0300

    create routes file

--- src/app.js
+++ src/app.js
@@ -5,19 +5,10 @@ const app = express();
 
 consign({ cwd: 'src', verbose: false })
   .include('./config/middlewares.js')
+  .then('./routes')
+  .then('./config/routes.js')
   .into(app);
 
 app.get('/', (req, res) => res.status(200).send());
-
-app.get('/users', (req, res) => {
-  const users = [
-    { name: 'Arthur Enrique', email: 'arthur@mail.com' },
-  ];
-  res.status(200).json(users);
-});
-
-app.post('/users', (req, res) => {
-  res.status(201).json(req.body);
-});
 
 module.exports = app;
